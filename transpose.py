3be267b08b6c3ad627a10a20b5f09e421c21d3e8
LabbyLabs2 <mari_f_bailey@yahoo.com>
2018-09-30 14:40:08 -0400

    Adding a cheesy comment

--- transpose.py
+++ transpose.py
@@ -9,3 +9,4 @@ with open('transposecsv.csv') as f:
 #find index of header, then get the same index position for each row
 Trying to add text
 Adding yet another comment
+#I like cheese. There are many types of cheese in the world, some types are mild, others are strong and smelly. They can be used in cooking or are good on top of crackers as an appetizer. What is the best way to store cheese?
