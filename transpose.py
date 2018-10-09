5158a131ed6557df74efa8067ef3612e19a98c8f
LabbyLabs2 <mari_f_bailey@yahoo.com>
2018-10-08 21:05:59 -0400

    lalala

--- transpose.py
+++ transpose.py
@@ -4,8 +4,11 @@ data = []
 
 print('hello there!!!')
 print(2+5)
+print('where is the cat?')
+print('I am not sure')
 
 #find index of header, then get the same index position for each row
 #Trying to add text
 #Adding yet another comment
 #I like cheese. There are many types of cheese in the world, some types are mild, others are strong and smelly. They can be used in cooking or are good on top of crackers as an appetizer. What is the best way to store cheese?
+i made some more big changes to the file
