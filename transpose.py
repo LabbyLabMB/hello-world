d2a5fc43cb3fc0d3e0c4e4e60346d265dc72bf9d
LabbyLabs2 <mari_f_bailey@yahoo.com>
2018-10-08 20:26:26 -0400

    I made some big changes to my file

--- transpose.py
+++ transpose.py
@@ -1,12 +1,11 @@
 import csv
 
 data = []
-with open('transposecsv.csv') as f:
-    for row in csv.reader(f):
-        data.append(row)
-    print(data)
+
+print('hello there!!!')
+print(2+5)
 
 #find index of header, then get the same index position for each row
-Trying to add text
-Adding yet another comment
+#Trying to add text
+#Adding yet another comment
 #I like cheese. There are many types of cheese in the world, some types are mild, others are strong and smelly. They can be used in cooking or are good on top of crackers as an appetizer. What is the best way to store cheese?
